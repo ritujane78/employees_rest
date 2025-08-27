e2ca26b9eb5893176fbb7cc3e91527523f841638
Ritu Bafna <ritujane78@gmail.com>
2025-08-27 17:04:40 +0545

    connect CRUD from controller to show in swagger UI, add screenshots

--- src/main/java/com/jane/springboot/employees/controller/EmployeeController.java
+++ src/main/java/com/jane/springboot/employees/controller/EmployeeController.java
@@ -2,16 +2,21 @@ package com.jane.springboot.employees.controller;
 
 import com.jane.springboot.employees.DAO.EmployeeDAO;
 import com.jane.springboot.employees.entity.Employee;
+import com.jane.springboot.employees.request.EmployeeRequest;
 import com.jane.springboot.employees.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
+@Tag(name = "Employees Rest API", description = "Operations related to employees")
 public class EmployeeController {
 
     EmployeeService employeeService;
@@ -21,10 +26,43 @@ public class EmployeeController {
         employeeService = theEmployeeService;
     }
 
+    @Operation(summary = "Get all employees", description = "Retrieve a list of all employees.")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<Employee> getEmployees(){
         List<Employee> employees = employeeService.findAll();
 
         return employees;
+    }
+
+    @Operation(summary = "Fetch single employee", description = "Get single employee from database")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable @Min(1) int id){
+        return employeeService.getById(id);
+    }
+
+    @Operation(summary = "Create a new employee", description = "Add a new employee to database.")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping()
+    public Employee addEmployee(@Valid @RequestBody EmployeeRequest employeeRequest){
+        Employee employee = employeeService.save(employeeRequest);
+        return employee;
+    }
+
+    @Operation(summary = "Update an employee", description = "Update the details of an existing employee")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{employeeId}")
+    public Employee updateEmployee(@PathVariable @Min(value=1) long employeeId,
+                                   @Valid @RequestBody EmployeeRequest employeeRequest){
+        Employee dbEmployee = employeeService.update(employeeId,employeeRequest);
+        return dbEmployee;
+    }
+
+    @Operation(summary = "Delete an employee", description = "Delete an employee from the database")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{employeeId}")
+    public void deleteEmployee(@PathVariable @Min(value = 1) long employeeId){
+        employeeService.deleteById(employeeId);
     }
 }
--- src/main/java/com/jane/springboot/employees/request/EmployeeRequest.java
+++ src/main/java/com/jane/springboot/employees/request/EmployeeRequest.java
@@ -1,13 +1,23 @@
 package com.jane.springboot.employees.request;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class EmployeeRequest {
 
 
+    @NotNull(message = "First Name is mandatory.")
+    @Size(min=2, max=50, message = "First name must be in between 2 to 50 characters")
     private String firstName;
 
+    @NotNull(message = "Last name is mandatory.")
+    @Size(min=2, max=50, message = "Last name must be in between 2 to 50 characters")
     private String lastName;
 
+    @NotNull(message = "Email is mandatory.")
+    @Email
     private String email;
 
     public EmployeeRequest(String firstName, String lastName, String email) {
