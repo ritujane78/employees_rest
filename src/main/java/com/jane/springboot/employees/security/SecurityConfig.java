d27f71a6060673726f59f81908bb3db793cd2d73
Ritu Bafna <ritujane78@gmail.com>
2025-08-29 16:33:28 +0545

    authenticate and authorize with swagger

--- src/main/java/com/jane/springboot/employees/security/SecurityConfig.java
+++ src/main/java/com/jane/springboot/employees/security/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.jane.springboot.employees.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -37,7 +39,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
             configurer
-                    .requestMatchers("/docs/**", "/swagger-ui/**","v3/api-docs/**","swagger-ui.html/**").permitAll()
+                    .requestMatchers("/docs/**", "/swagger-ui/**","/v3/api-docs/**","swagger-ui.html/**").permitAll()
                     .requestMatchers(HttpMethod.GET,"/api/employes").hasRole("EMPLOYEE")
                     .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
                     .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
@@ -45,11 +47,28 @@ public class SecurityConfig {
                     .requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
         );
 
+        http.httpBasic(httpBasicCustomizer -> httpBasicCustomizer.disable());
+
 //        Use http Basic Authenticaion
         http.httpBasic(Customizer.withDefaults());
 
         http.csrf(csrf -> csrf.disable());
 
+        http.exceptionHandling(exceptionHandling -> exceptionHandling
+                .authenticationEntryPoint(authenticationEntryPoint()));
+
         return http.build();
+    }
+
+    public AuthenticationEntryPoint authenticationEntryPoint(){
+        return (request, response, authException) ->{
+          response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+          response.setContentType("application/json");
+
+          response.setHeader("WWW-AUTHENTICATE", "");
+
+          response.getWriter().write("{\"error\":\"Unauthorised access\"}");
+        };
     }
 }
